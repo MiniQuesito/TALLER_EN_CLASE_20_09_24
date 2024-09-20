719bfd774f1d469636eee664408a8246fe49f457
Arnold Retavisca <105889999+MiniQuesito@users.noreply.github.com>
2024-09-20 12:12:45 -0500

    Creamos variables de acceso
    Creamos las variables con las cuales se trabaja en el metodo

--- Clase_Estructura_20_09_24/src/App.java
+++ Clase_Estructura_20_09_24/src/App.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+
 public class App {
+
+
+
+    
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        
+
+
+        Scanner entrada = new Scanner (System.in);
+
+        System.out.println("Ingrese el numero de su cedula: ");
+        int numcedula = entrada.nextInt();
+
+        System.out.println("Su numero de cedula es: " + numcedula);
+
+
+
+
+
+       
+
     }
+
+
 }
