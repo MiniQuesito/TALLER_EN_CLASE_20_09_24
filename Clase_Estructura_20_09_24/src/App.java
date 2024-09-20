c9db291eefefee2dee4cb5b6eeca52f78b1bd6cd
Arnold Retavisca <105889999+MiniQuesito@users.noreply.github.com>
2024-09-20 12:38:33 -0500

    Finaliza la entrega de la actividad
    Cumple con lo propuesto en clase

--- Clase_Estructura_20_09_24/src/App.java
+++ Clase_Estructura_20_09_24/src/App.java
@@ -2,6 +2,12 @@ import java.util.Scanner;
 
 public class App {
 
+    //Variables globales
+    
+    static String con1 = "1";
+    static String con2 = "2";
+    static String con3 = "3";
+ 
 
 
     
@@ -12,17 +18,31 @@ public class App {
         Scanner entrada = new Scanner (System.in);
 
         System.out.println("Ingrese el numero de su cedula: ");
-        int numcedula = entrada.nextInt();
+        String numcedula = entrada.nextLine();
 
         System.out.println("Su numero de cedula es: " + numcedula);
 
 
 
-
+       cedula(numcedula);
 
        
 
     }
+ 
+    public static String cedula(String num){
+         
+        if (num.contains(con1)){
+        
+            System.out.println("Su puesto de votacion es en Soacha");
+            
+        }else if (num.startsWith(con2)){
+            System.out.println("Su puesto de votacion es en Funza");
+        }
+        else if(num.contains(con3)) {
+            System.out.println("Su puesto de votación es en madrid");
+        }
+        return num;
 
-
+    }
 }
